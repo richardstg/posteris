247977c2ec6860a2d06b8a4a7a20976978e54a43
unknown <Richard Stg>
2021-02-11 11:52:22 +0000

    White logo

--- src/components/shared/image/logo.js
+++ src/components/shared/image/logo.js
@@ -5,7 +5,7 @@ import Img from "gatsby-image"
 const Logo = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "logo.png" }) {
+      placeholderImage: file(relativePath: { eq: "logo_white.png" }) {
         childImageSharp {
           fluid(quality: 100) {
             ...GatsbyImageSharpFluid
