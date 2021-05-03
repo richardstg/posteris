29412de77ab75e994d25ed615132b6a57ea94349
unknown <Richard Stg>
2021-05-03 13:35:38 +0100

    Add mont heavy font, add test page

---
+++ src/pages/testpage123.js
@@ -0,0 +1,23 @@
+import React from "react"
+// import SEO from "../components/shared/seo/seo"
+import Layout from "../hoc/layout/layout"
+import Intro from "../components/index/intro/intro"
+import OurBeliefs from "../components/index/ourBeliefs/ourBeliefs"
+import WhoWeAre from "../components/index/whoWeAre/whoWeAre"
+import OrganizationalCapitalEvolution from "../components/index/organizationalCapitalEvolution/organizationalCapitalEvolution"
+import Inquiries from "../components/index/inquiries/inquiries"
+
+const TestPage = () => {
+  return (
+    <Layout>
+      {/* <SEO title="POSTERIS" description="" /> */}
+      <Intro />
+      <OurBeliefs />
+      <WhoWeAre />
+      <OrganizationalCapitalEvolution />
+      <Inquiries />
+    </Layout>
+  )
+}
+
+export default TestPage
