d517e517d48337f2fe4523fead16edb954c9d333
unknown <Richard Stg>
2021-10-22 12:48:48 +0200

    Update privacy policy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,13 +1,14 @@
 import React from "react"
 import SEO from "../components/shared/seo/seo"
 import GdprContent from "../components/gdpr/gdprcontent"
+import Layout from "../hoc/layout/layout"
 
 const GdprPage = () => {
   return (
-    <React.Fragment>
-      <SEO title="POSTERIS" description="Integritetspolicy" />
+    <Layout>
+      <SEO title="POSTERIS" description="Privacy policy" />
       <GdprContent />
-    </React.Fragment>
+    </Layout>
   )
 }
 
